f7d06b8b378f874810a5ce88ef893e6ea090814e
Sturla <nicolas.sturla@sofse.gob.ar>
2023-12-10 23:14:22 -0300

    project

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,29 +1,24 @@
 <template lang="pug">
 
   .container    
-    .field.is-horizontal
-      .field-label.is-normal
-        label.label Titulo
-      .field-body
-        .field
-          p.control
+    .field      
+        label.label.has-text-left.is-size-5 Titulo      
+        .control
             input.input.is-medium.is-rounded(v-model="title", type="text" placeholder="Titulo")
             span.is-small {{ title }}
 
-    .field.is-horizontal
-      .field-label.is-normal
-        label.label Tiempo
-      .field-body
-        .field
-          p.control
+    .field      
+        label.label.has-text-left.is-size-5 Tiempo
+        .control
             input.input.is-medium(v-model="time", type="number" placeholder="Tiempo")
             span.is-small {{ time }}
-   
+    
+      
     .field.is-grouped
-      p.control
-        button.button.is-success.is-medium(@click="addNewTask") Nueva Tarea
-      p.control
-        button.button.is-danger.is-medium(@click="cancelar") Cancelar
+      .control
+          button.button.is-success.is-medium(@click="addNewTask") Nueva Tarea
+      .control
+          button.button.is-danger.is-medium(@click="cancelar") Cancelar
     .field
       .label.label  {{ totalTime }}   
   
